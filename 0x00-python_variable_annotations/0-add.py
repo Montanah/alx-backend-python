d20d14b561f6c139297116562274758224ef818d
Montanah <mititi.isaac@gmail.com>
2023-08-04 22:56:44 +0000

    Python varibale annotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,9 +2,9 @@
 """type-annotated function """
 
 
-def add(a: float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """
-    Function add that takes a float a and a float b 
+    Function add that takes a float a and a float b
     as arguments and returns their sum as a float
     """
     sum = a + b
