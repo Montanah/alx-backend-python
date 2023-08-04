052f37ef9c769e31e492ca141d169fafb780beb5
Montanah <mititi.isaac@gmail.com>
2023-08-04 23:05:21 +0000

    Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 """type-annotated function"""
 
 
-def concat(str1: str, str2: str) ->str:
+def concat(str1: str, str2: str) -> str:
     """
     function concat that takes a string str1 and a string
     str2 as arguments and returns a concatenated string
