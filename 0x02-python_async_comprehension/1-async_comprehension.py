6366db6afbda6a195472fac891bd72b01c4979b1
Montanah <mititi.isaac@gmail.com>
2023-08-08 14:35:07 -0700

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Async Comprehensions """
+
+import asyncio
+import random
+
+from typing import Generator, List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Async Comprehension """
+    return [i async for i in async_generator()]
+
+async def main() -> None:
+    """ Main Function """
+    print(await async_comprehension())
