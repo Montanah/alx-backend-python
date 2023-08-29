af23b41d25cff0dd5e712cd21f781e5e53776843
Montanah <mititi.isaac@gmail.com>
2023-08-29 10:09:53 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,17 +34,18 @@ class TestGithubOrgClient(unittest.TestCase):
                              mock_public_repos_url.return_value)
             mock_public_repos_url.assert_called_once()
 
-    @parameterized.expand([
-        (org, repos, apache2)
-        for org, repos, _, apache2 in zip(org_payload, repos_payload, expected_repos, apache2_repos)
-    ])
-    @patch('requests.get', side_effect=[org_payload, repos_payload])
-    def test_public_repos(self, org_payload, repos_payload, expected_repos, apache2_repos, mock_get):
-        mock_get.side_effect = [org_payload, repos_payload]
-        
-        test_class = GithubOrgClient('test')
-        repos = test_class.public_repos(license="apache-2.0")
-        self.assertEqual(repos, apache2_repos)
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ Test public_repos method """
+        mock_get_json.return_value = TEST_PAYLOAD
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            test_class = GithubOrgClient('test')
+            self.assertEqual(test_class.public_repos(),
+                             mock_get_json.return_value)
+            mock_get_json.assert_called_once_with
+            (mock_public_repos_url.return_value)
+            mock_public_repos_url.assert_called_once()
 
     def test_public_repos_with_license(self):
         """ Test public_repos method with license """
@@ -67,10 +68,9 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_class.has_license(repo, license_key), expected)
 
 
-@parameterized_class([
-    {"org_payload": org_payload, "repos_payload": repos_payload,
-     "expected_repos": expected_repos, "apache2_repos": apache2_repos}
-])
+@parameterized_class([{"org_payload": org_payload, "repos_payload":
+                     repos_payload, "expected_repos": expected_repos,
+                     "apache2_repos": apache2_repos}])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """ TestIntegrationGithubOrgClient class """
 
